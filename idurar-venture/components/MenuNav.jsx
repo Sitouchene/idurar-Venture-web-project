19965036c12a9f8eacd8dc10e5daa874f15c261a
metabteb2715151 <149700991+metabteb2715151@users.noreply.github.com>
2024-05-28 14:22:49 -0400

    menu nav responsive

--- idurar-venture/components/MenuNav.jsx
+++ idurar-venture/components/MenuNav.jsx
@@ -1,37 +1,44 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { FaHome, FaHiking, FaCalendarAlt } from "react-icons/fa";
-import Randonnee from './Randonnee';
-
-
 import styles from './MenuNav.module.css';
 
+export default function MenuNav() {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-export default function MenuNav({setPage}) {
-    return <nav className={styles.nav}>
-    <ul className={styles.menu}>
-      <li><a href="/"><FaHome /> Accueil</a></li>
-      <li>
-        <a href=""><FaHiking /> Activités</a>
-        <ul className={styles.submenu}>
-         
-        
-          <li> <a href="#" onClick={() => setPage('Randonnee')}>Randonnee</a></li>
-          <li><a href="/">Chasse</a></li>
-          <li><a href="/">Parapente</a></li>
-          <li><a href="/">Autres</a></li>
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-        </ul>
-      </li>
-      <li>
-        <a href="/"><FaCalendarAlt /> Evenements</a>
-        <ul className={styles.submenu}>
-          <li><a href="/">Festivals</a></li>
-          <li><a href="/">Processions religieuses</a></li>
-          <li><a href="/">Twiza</a></li>
-        </ul>
-      </li>
-      <li><a href="/">Feedback</a></li>
-    </ul>
-  </nav>
+    return (
+        <nav className={styles.nav}>
+            <div className={styles['burger-menu']} onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <ul className={`${styles.menu} ${menuOpen ? styles.show : ''}`}>
+                <li><a href="/"><FaHome /> Accueil</a></li>
+                <li>
+                    <a href="#"><FaHiking /> Activités</a>
+                    <ul className={styles.submenu}>
+                        <li><a href="/">Randonnée</a></li>
+                        <li><a href="/">Chasse</a></li>
+                        <li><a href="/">Parapente</a></li>
+                        <li><a href="/">Autres</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/"><FaCalendarAlt /> Evenements</a>
+                    <ul className={styles.submenu}>
+                        <li><a href="/">Festivals</a></li>
+                        <li><a href="/">Processions religieuses</a></li>
+                        <li><a href="/">Twiza</a></li>
+                    </ul>
+                </li>
+                <li><a href="/">Feedback</a></li>
+            </ul>
+        </nav>
+    );
 }
-
